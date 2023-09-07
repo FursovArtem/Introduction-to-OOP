108e77fac7d201a20800c7446dc94108f094556f
Artem <aztec@DESKTOP-CK171NA>
2023-09-07 17:17:03 +0400

    string separation

---
+++ String/String.cpp
@@ -0,0 +1,102 @@
+#include "String.h"
+String operator+(const String& lvalue, const String& rvalue);
+
+int String::get_size()const
+{
+	return size;
+}
+const char* String::get_str()const
+{
+	return str;
+}
+char* String::get_str()
+{
+	return str;
+}
+// Constructors:
+
+// ключевое слово explicit можно писать только внутри класса
+String::String(int size) : size(size), str(new char[size] {})
+{
+	// default
+	//this->size = size;
+	//this->str = new char[size] {};
+}
+String::String(const char* str) : String(strlen(str) + 1)
+{
+	//this->size = strlen(str) + 1;
+	//this->str = new char[size] {};
+	for (int i = 0; str[i]; i++)this->str[i] = str[i];
+}
+String::String(const String& other)noexcept : String(other.str)
+{
+	//Deep copy
+	//this->size = other.size;
+	//this->str = new char[size] {};
+	//for (int i = 0; i < size; i++)this->str[i] = other.str[i];
+}
+String::String(String&& other)noexcept : size(other.size), str(other.str)
+{
+	//Shallow copy
+	//this->size = other.size;
+	//this->str = other.str;
+	other.size = 0;
+	other.str = nullptr;
+}
+String::~String()
+{
+	delete[] str;
+}
+
+// Operators:
+
+String& String::operator=(const String& other)noexcept
+{
+	if (this == &other)return *this;
+	delete[] this->str;
+	this->size = other.size;
+	this->str = new char[size] {};
+	for (int i = 0; i < size; i++)this->str[i] = other.str[i];
+	return *this;
+}
+String& String::operator=(String&& other)noexcept
+{
+	if (this == &other) return *this;
+	this->size = other.size;
+	this->str = other.str;
+	other.size = 0;
+	other.str = nullptr;
+}
+String& String::operator+=(const String& rvalue)
+{
+	return *this = *this + rvalue;
+}
+
+char String::operator[](int i)const
+{
+	return str[i];
+}
+char& String::operator[](int i)
+{
+	return str[i];
+}
+
+// Methods:
+
+void String::print()const
+{
+	cout << "Size:\t" << size << endl;
+	cout << "Str:\t" << str << endl;
+}
+
+String operator+(const String& lvalue, const String& rvalue)
+{
+	String result(lvalue.get_size() + rvalue.get_size() - 1);
+	for (int i = 0; i < lvalue.get_size(); i++)result[i] = lvalue[i];
+	for (int i = 0; i < rvalue.get_size(); i++)result[lvalue.get_size() - 1 + i] = rvalue[i];
+	return result;
+}
+std::ostream& operator <<(std::ostream& os, const String& obj)
+{
+	return os << obj.get_str();
+}
---
+++ String/String.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <iostream>
+using std::cout;
+using std::endl;
+
+class String
+{
+private:
+	int size;
+	char* str;
+public:
+	int get_size()const;
+	const char* get_str()const;
+	char* get_str();
+	// Constructors:
+
+	explicit String(int size = 80);
+	String(const char* str);
+	String(const String& other)noexcept;
+	String(String&& other)noexcept;
+	~String();
+
+	// Operators:
+
+	String& operator=(const String& other)noexcept;
+	String& operator=(String&& other)noexcept;
+	String& operator+=(const String& rvalue);
+
+	char operator[](int i)const;
+	char& operator[](int i);
+
+	// Methods:
+
+	void print()const;
+};
